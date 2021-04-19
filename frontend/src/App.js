4fd6bb29a42532b91c110c3debbe6efb20a9325b
Deon Odendaal <deon.odendaal@pm.me>
2021-04-19 19:14:54 +0100

    implement react-router-dom and react-router-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
-// import logo from './logo.svg';
-// import "./App.css";
-import { Container } from 'react-bootstrap';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Homescreen from './screens/Homescreen';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Homescreen from "./screens/Homescreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <Homescreen />
-        </Container>
-      </main>
-      <Footer />
-    </>
+    <Router>
+      <>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Route path='/' component={Homescreen} exact />
+            <Route path='/product/:id' component={ProductScreen} />
+          </Container>
+        </main>
+        <Footer />
+      </>
+    </Router>
   );
 };
 
