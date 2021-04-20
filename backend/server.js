a84cb965b3c34da5fe441f29154ce3c435d6021f
Deon Odendaal <deon.odendaal@protonmail.com>
2021-04-20 19:09:46 +0100

    environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
 
 // initialize express
 const app = express();
@@ -22,4 +25,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('Server listining on port 5000'));
+const PORT = process.env.PORT || 5000;
+// run the server
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
