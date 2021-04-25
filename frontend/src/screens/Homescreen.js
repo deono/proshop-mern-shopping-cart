2a7ea347eb6928375e8fefa835ca7dd9fdb78854
Deon Odendaal <deon.odendaal@protonmail.com>
2021-04-25 19:46:54 +0100

    bringing redux state into homescreen with useSelector and useDispatch

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-// import products from '../products';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'; // uses react hooks instead of higer order functions (connect)
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import { listProducts } from '../actions/productActions';
 
 const Homescreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  const productList = useSelector(state => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    // to async / await syntax, create a function within useEffect
-    const fetchProducts = async () => {
-      // fetch the products array from the API
-      const { data } = await axios.get('/api/products');
-      // set the products array in component state
-      setProducts(data);
-    };
-
-    // call the function
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map(product => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map(product => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
