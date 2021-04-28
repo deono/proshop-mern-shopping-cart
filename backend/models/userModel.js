95b3d5aa1e9bea93649ae184131c6db7cb124daf
Deon Odendaal <deon.odendaal@pm.me>
2021-04-28 13:44:27 +0100

    user registration and password encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,37 +1,48 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      required: true
+      required: true,
     },
     email: {
       type: String,
       required: true,
-      unique: true
+      unique: true,
     },
     password: {
       type: String,
-      required: true
+      required: true,
     },
     isAdmin: {
       type: Boolean,
       required: true,
-      default: false
-    }
+      default: false,
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
 
-userSchema.methods.matchPassword = async function(enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
   // compare the plaintext password with hashed password in the database
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
+// excrypt the password before saving the user data
+userSchema.pre("save", async function (next) {
+  // if the password field is not modified, continue
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,17 @@
 import express from "express";
 const router = express.Router();
 // all functionality is in controller file
-import { authUser, getUserProfile } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+router.route("/").post(registerUser);
 
 // @desc    Authenticate user and get token
 // @route   POST /api/users/login
