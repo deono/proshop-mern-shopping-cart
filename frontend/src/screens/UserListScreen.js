c90ac2a2b45bc0fed29abfa479af96bcbf358096
Deon Odendaal <deon.odendaal@protonmail.com>
2021-05-23 04:47:58 +0100

    admin screen access sceurity

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,23 +1,30 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Button, Table } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import Message from "../components/Message";
-import Loader from "../components/Loader";
-import { listUsers } from "../actions/userActions";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Table } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { listUsers } from '../actions/userActions';
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
 
-  const userList = useSelector((state) => state.userList);
+  const userList = useSelector(state => state.userList);
   const { loading, error, users } = userList;
 
-  useEffect(() => {
-    dispatch(listUsers());
-  }, [dispatch]);
+  const userLogin = useSelector(state => state.userLogin);
+  const { userInfo } = userLogin;
 
-  const deleteHandler = (id) => {
-    console.log("deleteHandler called", id);
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      history.push('/login');
+    }
+  }, [dispatch, history, userInfo]);
+
+  const deleteHandler = id => {
+    console.log('deleteHandler called', id);
   };
 
   return (
@@ -39,7 +46,7 @@ const UserListScreen = () => {
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
+            {users.map(user => (
               <tr key={user._id}>
                 <td>
                   <Link to={`/user/${user._id}/edit`}>{user._id}</Link>
@@ -50,9 +57,9 @@ const UserListScreen = () => {
                 </td>
                 <td>
                   {user.isAdmin ? (
-                    <i className='fas fa-check' style={{ color: "green" }}></i>
+                    <i className='fas fa-check' style={{ color: 'green' }}></i>
                   ) : (
-                    <i className='fas fa-times' style={{ color: "red" }}></i>
+                    <i className='fas fa-times' style={{ color: 'red' }}></i>
                   )}
                 </td>
                 <td>
