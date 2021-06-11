d796723a2831bf9b04b55cec57aa286abc5f3e0f
Deon Odendaal <deon.odendaal@pm.me>
2021-06-11 11:46:08 +0100

    logging

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import path from "path";
 import express from "express";
 import { config } from "dotenv";
 import colors from "colors";
+import morgan from "morgan";
+import rfs from "rotating-file-stream";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js"; // add the .js extention when using ES Modules (import syntax)
 
@@ -19,6 +21,64 @@ connectDB();
 
 // initialize express
 const app = express();
+
+// initialize __dirname - equired when using ES modules syntax
+const __dirname = path.resolve();
+
+/*
+======================================================================
+LOGGING
+======================================================================
+*/
+
+// filename generator
+const pad = (num) => (num > 9 ? "" : "0") + num;
+const generator = () => {
+  let date = new Date();
+
+  let year = date.getFullYear();
+  let month = pad(date.getMonth() + 1);
+  let day = pad(date.getDate());
+
+  return `${year}${month}/${year}-${month}-${day}_accesslog.log`;
+};
+
+// create a rotating write stream
+var accessLogStream = rfs.createStream(generator, {
+  interval: "1d", // rotate daily
+  path: path.join(__dirname, "logs"),
+});
+
+// morgan logger - used only in dev mode
+// setup the logger
+
+app.use(
+  morgan(
+    function (tokens, req, res) {
+      return [
+        tokens.date(req, res, "iso"),
+        tokens["http-version"](req, res),
+        tokens["remote-addr"](req, res),
+        tokens.referrer(req, res),
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens["user-agent"](req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, "content-length"),
+        "-",
+        tokens["response-time"](req, res),
+        "ms",
+      ].join(" ");
+    },
+    { stream: accessLogStream }
+  )
+);
+
+/*
+======================================================================
+*******
+======================================================================
+*/
 
 // parse json data in the request body
 app.use(express.json());
@@ -39,8 +99,6 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-// make uploads folder accessible to browser
-const __dirname = path.resolve(); // required when using ES modules syntax
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // use middleware
